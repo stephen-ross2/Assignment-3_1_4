ce32b88391076c7c1dd5c48d80cad954ad7f2e3e
stephen-ross2 <stephen.ross2@outlook.com>
2024-11-18 13:47:15 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Assignment_3_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the X-Coordinate");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the Y-Coordinate");
+            double y = double.Parse(Console.ReadLine());
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("The point lies at the origin.");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("The point lies on the Y-axis.");
+            }
+            else if (y == 0)
+            {
+                Console.WriteLine("The point lies on the X-axis.");
+            }
+            else
+            {
+                // Determine which quadrant the point lies in
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("The point lies in Quadrant I.");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("The point lies in Quadrant II.");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("The point lies in Quadrant III.");
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("The point lies in Quadrant IV.");
+                }
+            }
+        }
+    }
+}
